76cc7a4a85b2ec5b9853983a10492f3659d901ff
marthamora <marthamora1235@gmail.com>
2017-06-15 23:10:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-
+import os 
 from biblioteca import biblioteca
+ruta_deimagenes = os.getcwd()  
 
-## Dame el directorio donde buscar imagenes
-
-directorio= "~/MisImagenes"
-
+directorio= "ruta_deimagenes"
 Bibloteca = biblioteca(directorio)
-
-
-
 Numerodeosos=Bibioteca.count([1,4,5,6])
 print(Numerodeosos)
-
-
 Biblioteca.show([1,3,5]);
-
 Biblioteca.showImagenes(1,40)
 
 
 
 
-#for (ruta, carpeta, imagen) in walk('ruta_deimagenes'):
-   # print ruta
-    #print carpeta
-    #print imagen
-
- ##  Lista_carpeta = [carpeta]
-    #Lista_imagen = [imagen]
-   # print Lista_imagen
